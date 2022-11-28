96dfa4b2b83067e7d71a6f13c1b8212ad3f38f00
Jason Wang <jasonherngwang@gmail.com>
2022-11-27 16:32:27 -0800

    test: add test for approach 2

--- leetcode/hashtables_arrays/lc49_group_anagrams/problem_test.go
+++ leetcode/hashtables_arrays/lc49_group_anagrams/problem_test.go
@@ -53,3 +53,48 @@ func Test_groupAnagrams(t *testing.T) {
 		})
 	}
 }
+
+func Test_groupAnagrams2(t *testing.T) {
+	type input struct {
+		strs []string
+	}
+
+	type testCase struct {
+		name     string
+		input    input
+		expected [][]string
+	}
+
+	tests := []testCase{
+		{
+			name: "Group anagrams",
+			input: input{
+				strs: []string{
+					"eat", "tea", "tan", "ate", "nat", "bat",
+				},
+			},
+			expected: [][]string{
+				{
+					"bat",
+				},
+				{
+					"eat", "tea", "ate",
+				},
+				{
+					"tan", "nat",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := groupAnagrams2(test.input.strs)
+			// Result order doesn't matter; sort by 1st element for this case
+			sort.Slice(actual, func(i int, j int) bool {
+				return actual[i][0] < actual[j][0]
+			})
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
