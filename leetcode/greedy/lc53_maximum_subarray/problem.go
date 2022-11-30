ae503bf3335561d037e81ddb68e38aaa42291c8d
Jason Wang <jasonherngwang@gmail.com>
2022-11-30 13:47:20 -0800

    feat: add dp soln

--- leetcode/greedy/lc53_maximum_subarray/problem.go
+++ leetcode/greedy/lc53_maximum_subarray/problem.go
@@ -38,20 +38,34 @@ Array and pointers
 Algorithm
 ------------------------------------------
 Time: O(N) for 1 traversal
-Space: O(1) for pointers
+Space: O(1) for some extra variables
 
-Use Kadane's algo:
+Approach 1: Kadane's algo, Greedy
 - If current value is greater than sum of all previous elements, then discard
   all those previous elements and start over with the current.
 
 Steps:
-- Initialize variable to track current sum and max observed sum.
+- Initialize variables to track current sum and max observed sum.
 - Iterate over array:
   - If elem > sum, discard previous elems.
     - Set sum to current elem.
   - Else, add current elem to current sum
   - Compare current and max sum; overwrite if larger
 - Return max sum
+
+Approach 2: Dynamic Programming
+Time: O(N) for 1 traversal
+Space: O(N) since we use an additional array to store sums
+
+Steps:
+- Initialize variable to track max observed sum.
+- Initialize array containing the first element from the input array. Each slot
+  will hold one of these, whichever is larger:
+  - The sum of the previous element and the value at the corresponding index in
+    the input array
+  - Just the value at the corresponding index in the input array
+- Iterate over the input array, filling in the new array.
+- Return max observed sum.
 */
 
 package leetcode
@@ -70,6 +84,19 @@ func maxSubArray(nums []int) int {
 	for _, n := range nums[1:] {
 		sum = max(n, sum+n)
 		maxSum = max(sum, maxSum)
+	}
+
+	return maxSum
+}
+
+func maxSubArrayDP(nums []int) int {
+	maxSum := nums[0]
+	sums := []int{nums[0]}
+
+	// i=0 means index 1 in sums; need to add 1 when accessing sums
+	for i, n := range nums[1:] {
+		sums = append(sums, max(n, sums[i]+n))
+		maxSum = max(sums[i+1], maxSum)
 	}
 
 	return maxSum
--- leetcode/greedy/lc53_maximum_subarray/problem_test.go
+++ leetcode/greedy/lc53_maximum_subarray/problem_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_largestSubsectionSum(t *testing.T) {
+func Test_maxSubArray(t *testing.T) {
 	type input struct {
 		nums []int
 	}
@@ -58,6 +58,63 @@ func Test_largestSubsectionSum(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			actual := maxSubArray(test.input.nums)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func Test_maxSubArrayDP(t *testing.T) {
+	type input struct {
+		nums []int
+	}
+
+	type testCase struct {
+		name     string
+		input    input
+		expected int
+	}
+
+	tests := []testCase{
+		{
+			name: "array of numbers",
+			input: input{
+				nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4},
+			},
+			expected: 6,
+		},
+		{
+			name: "array of numbers",
+			input: input{
+				nums: []int{5, 4, -1, 7, 8},
+			},
+			expected: 23,
+		},
+		{
+			name: "array of 1 number",
+			input: input{
+				nums: []int{1},
+			},
+			expected: 1,
+		},
+		{
+			name: "array of 0",
+			input: input{
+				nums: []int{0},
+			},
+			expected: 0,
+		},
+		{
+			name: "array of negative numbers",
+			input: input{
+				nums: []int{-1, -2, -3},
+			},
+			expected: -1,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := maxSubArrayDP(test.input.nums)
 			assert.Equal(t, test.expected, actual)
 		})
 	}
