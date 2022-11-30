7026f07a34c0010a9f81f2e4a15c473a24bf7515
Jason Wang <jasonherngwang@gmail.com>
2022-11-29 18:19:32 -0800

    test: add test for iterative solution

--- leetcode/binary_tree/lc104_max_depth_binary_tree/problem_test.go
+++ leetcode/binary_tree/lc104_max_depth_binary_tree/problem_test.go
@@ -58,3 +58,56 @@ func Test_maxDepth(t *testing.T) {
 		})
 	}
 }
+
+func Test_maxDepthIterative(t *testing.T) {
+	type input struct {
+		root *TreeNode
+	}
+
+	type testCase struct {
+		name     string
+		input    input
+		expected int
+	}
+
+	tests := []testCase{
+		{
+			name: "binary tree",
+			input: input{
+				root: &TreeNode{
+					Val: 5,
+					Left: &TreeNode{
+						Val:   1,
+						Right: &TreeNode{Val: 3},
+					},
+					Right: &TreeNode{
+						Val:  10,
+						Left: &TreeNode{Val: 7},
+					},
+				},
+			},
+			expected: 3,
+		},
+		{
+			name: "depth 1",
+			input: input{
+				root: &TreeNode{},
+			},
+			expected: 1,
+		},
+		{
+			name: "empty tree",
+			input: input{
+				root: nil,
+			},
+			expected: 0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := maxDepthIterative(test.input.root)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
